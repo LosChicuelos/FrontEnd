e719876d854cbb9a6998887c404cfb12aba76632
Bryam Diaz <bsdiaza@unal.edu.co>
2018-03-15 23:10:59 +0000

    asdas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
-import App from './components/Login';
+import App from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
