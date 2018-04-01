b3e46794c576aeb0d5753fe05e7968086942ff68
Bryam Diaz <bsdiaza@unal.edu.co>
2018-04-01 08:41:21 +0000

    validations ready

--- frontend/src/components/LoginInfo.js
+++ frontend/src/components/LoginInfo.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../style/LoginInfo.css';
 class LoginInfo extends Component {
+    
   render() {
+      
     return (
         <div className="col align-self-center" id="logincol">
             <div className="label" id="usuario">
                 <center><h1 className="labe">Usuario</h1></center>
-                <center><input  type="text" name="User" className="inputText" id="userText"/></center>
+                <center><input  type="text"id="user" name="Usuario" className="inputText"/></center>
             </div>
             <div className="label" id="pass">
                 <center><h1 className="labe">Contraseña</h1></center>
-                <center><input  type="text" name="Password" className="inputText" id="passText"/></center>
+                <center><input  type="text" id="pass" name="Contraseña" className="inputText"/></center>
             </div>
-            <center><button className="button button1">Ingresar</button></center> 
+            <center><button className="button button1" onClick={()=>this.validateLogin()}>Ingresar</button></center> 
         </div>
     );
+  }
+       validateLogin(){
+        var user = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
+        var pass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        this.validateInput(document.getElementById("user"),user);
+        this.validateInput(document.getElementById("pass"),pass);
+  }
+  
+  validateInput(input,list){
+      if(list.test(input.value)){
+            return true;
+        } else {
+            alert(input.id+ ' invalido');
+            return false;
+        }
   }
 }
 
--- frontend/src/components/SingupInfo.js
+++ frontend/src/components/SingupInfo.js
@@ -7,67 +7,78 @@ class SignupInfo extends Component {
             <div className="row" id="body-Singup">
                 <div className="row" id="row-1">
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Nombre</h1></center>
-                          <center><input  type="text" name="firstname" className="inputText SingupInput"/></center>
-                        </form> 
+                          <center><input  type="text" id="firstname" name="nombre" className="inputText SingupInput"/></center>
                     </div>
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Apellido</h1></center>
-                          <center><input  type="text" name="lastname" className="inputText SingupInput"/></center>
-                        </form> 
+                          <center><input  type="text" id="lastname" name="apellido" className="inputText SingupInput"/></center>
                     </div>
                 </div>
                 <div className="row" id="row-2">
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Identificación</h1></center>
-                          <center><input  type="text" name="id" className="inputText SingupInput"/></center>
-                        </form> 
+                          <center><input  type="text" id="id" name="identificacion" className="inputText SingupInput"/></center>
                     </div>
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Tipo identificación</h1></center>
                           <center><input  type="text" name="id-type" className="inputText SingupInput"/></center>
-                        </form> 
                     </div>
                 </div>
                 <div className="row" id="row-2">
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Correo</h1></center>
-                          <center><input  type="text" name="mail" className="inputText SingupInput"/></center>
-                        </form> 
+                          <center><input  type="text" id="email" name="correo" className="inputText SingupInput"/></center>
                     </div>
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Telefono</h1></center>
-                          <center><input  type="text" name="cell" className="inputText SingupInput"/></center>
-                        </form> 
+                          <center><input  type="text" id="cellphone" name="telefono" className="inputText SingupInput"/></center>
                     </div>
                 </div>
                 <div className="row" id="row-2">
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Usuario</h1></center>
-                          <center><input  type="text" name="user" className="inputText SingupInput"/></center>
-                        </form> 
+                          <center><input  type="text" id="user" name="usuario" className="inputText SingupInput"/></center>
                     </div>
                     <div className="col ">
-                        <form action="/action_page.php">
                           <center><h1 className="labe">Contraseña</h1></center>
-                          <center><input  type="text" name="password" className="inputText SingupInput"/></center>
-                        </form> 
+                          <center><input  type="text" id="pass" name="contraseña" className="inputText SingupInput"/></center>
                     </div>
                 </div>
                 <div className="col" id="row-2">
-                    <center><button className="button button1">Registar</button></center>
+                    <center><button className="button button1"onClick={()=>this.validateSingup()}>Registar</button></center>
                 </div>
             </div>
       
     );
   }
+     validateSingup(){
+        var string = /^[a-zA-Z]+$/;
+        var number = /^[0-9]+$/;
+        var cellphone = /^\d{10}$/;
+        var user = /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/;
+        var pass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        var email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        this.validateInput(document.getElementById("firstname"),string);
+        this.validateInput(document.getElementById("lastname"),string);
+        this.validateInput(document.getElementById("id"),number);
+        this.validateInput(document.getElementById("email"),email);
+        this.validateInput(document.getElementById("cellphone"),cellphone);    
+        this.validateInput(document.getElementById("user"),user);
+        this.validateInput(document.getElementById("pass"),pass);
+        
+        
+  }
+  
+  validateInput(input,list){
+      if(list.test(input.value)){
+            return true;
+        } else {
+            alert(input.name+ ' invalido');
+            return false;
+        }
+  }
+
 }
 
 export default SignupInfo;
