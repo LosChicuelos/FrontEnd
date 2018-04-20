88de4536d39bd479fd19ae6c5f1b607a180a6ffe
Juan D. Gaitan <jdgaitanf@unal.edu.co>
2018-04-20 03:28:50 -0500

    Vista Producto

--- frontend/src/components/AddProductInfo.js
+++ frontend/src/components/AddProductInfo.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import '../style/ProductInfo.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 class AddProductInfo extends Component {
   render() {
     return (        
-    <div className="col" id="bodycontainer">
-        <div className="col" id ="DetailsCol">
+      <div className="col" id="body-Singup" align="center">
+        
+
+          <form method="POST" action="https://backend-bsdiaza.c9users.io/articles" >
+            <div className="col">
+              <br/><h1 className="labe">Nombre</h1>
+              <input type="text" name="name" placeholder="Nombre" className="inputText SingupInput"/>
+            </div>
+            <div className="col">
+              <h1 className="labe">Precio</h1>
+              <input type="text" name="price" placeholder="Precio" className="inputText SingupInput"/>
+            </div>
+            <div className="col">
+              <h1 className="labe">Inventario</h1>
+              <input type="text" name="inventory" placeholder="Inventario" className="inputText SingupInput"/>            
+            </div>
+            <div className="col">
+              <h1 className="labe">Descripción</h1>
+              <textarea name="description" placeholder="Descripción" className="inputText SingupInput"></textarea>
+            </div>
+            <div className="col">
+              <h1 className="labe">Foto</h1>
+              <input type="file" name="fileToUpload" id="fileToUpload" className="inputText SingupInput"/>
+            </div>
+            <div className="col">
+              
+              <button type="submit" className="inputText SingupInput">Send</button>
+            </div>
             
-        </div>
+          </form>
+
+            
+        
     </div>
+
+    
     );
   }
 }
