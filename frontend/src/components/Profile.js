cf54ac442d0948a4d8dc1876d66fff9628f5e8e9
Juan D. Gaitan <jdgaitanf@unal.edu.co>
2018-04-20 06:37:25 -0500

    Arreglada vista Producto

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../style/Home.css';
 import Header from './Header';
+import ProfileInfo from './ProfileInfo';
 import BodyHome from './BodyHome'
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
       <div className="container-full" id ="screen">
         <div className="row" id ="display">
   				<Header value='complete'/>
+          <ProfileInfo/>
         </div>
       </div>
       
---
+++ frontend/src/components/ProfileInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/ProfileInfo.css';
+class ProfileInfo extends Component {
+  render() {
+    return (        
+    <div className="col" id="bodycontainer">
+        <div className="col" id ="DetailsCol">
+            
+        </div>
+    </div>
+    );
+  }
+}
+export default ProductInfo;
